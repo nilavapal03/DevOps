c5f89911609d991eb4eb8bbd498d169908a56d7c
NilavaPal <palnilava03@gmail.com>
2020-05-17 21:44:04 +0530

    DevOps project created By Nilava
    date: 5/17/2020

---
+++ DevOpsProject/src/test/java/com/nilava/DevOps/AppTest.java
@@ -0,0 +1,17 @@
+package com.nilava.DevOps;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    public static void main(String[] args) {
+		System.out.println("Hello World");
+		
+		
+	}
+}
